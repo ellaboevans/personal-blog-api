edd369af9d7c8f981a06d29dd2264aeb1666afc6
Evans <ellaboevans@gmail.com>
2023-10-02 12:20:46 +0000

    Fixing cors

--- utils/server.js
+++ utils/server.js
@@ -13,7 +13,7 @@ const createServer = () => {
   const app = express()
 
   //Middlewares
-  app.use(cors({ credentials: true, origin: 'https://oxconcept.vercel.app/' }))
+  app.use(cors({ credentials: true }))
   app.use(morgan('short'))
   app.use(express.json())
   app.use(bodyParser.urlencoded({ extended: false }))
